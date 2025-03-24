e20986892e3e4155bc1e29bfeb7cf6868707efd8
Rabia Bashir <x_rabba@berzelius1.nsc.liu.se>
2025-03-24 11:19:46 +0100

    Slurm job submit is successful.

--- run_sif.sh
+++ run_sif.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Paths
-CONTAINER=/proj/gcae_berzelius/users/x_rabba/pytorch_25.01-py3.sif
+# CONTAINER=/proj/gcae_berzelius/users/x_rabba/pytorch_25.01-py3.sif
+CONTAINER=/proj/gcae_berzelius/users/x_rabba/lightning_25.01-py3.sif
 PROJECT_DIR=/proj/gcae_berzelius/users/x_rabba/GenomeDiffusion
 DATA_DIR=/proj/gcae_berzelius/users/shared/HO_data
 
 # Run inside Apptainer, ensuring "data/" is mapped correctly
 apptainer exec --nv -B $PROJECT_DIR:/workspace -B $DATA_DIR:/workspace/data \
-$CONTAINER bash -c "cd /workspace && python train.py --config config.yaml"
+$CONTAINER bash -c "cd /workspace && python run_sif.py --config config.yaml"
 
